44cca77071b0a5a8e0134f59bad75831abc807ca
SENAI <ggnespoli@gmail.com>
2023-03-15 21:27:02 -0300

    feat: absolute paths

--- my-vue-app/vite.config.ts
+++ my-vue-app/vite.config.ts
@@ -1,7 +1,17 @@
+import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      types: `${path.resolve(__dirname, "./src/@types")}`,
+      modules: `${path.resolve(__dirname, "./src/modules/")}`,
+    },
+  },
 })
